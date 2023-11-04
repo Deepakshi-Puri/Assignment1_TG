37953472a7b21c826bc6df383bcf4d87d4a17103
Deepakshi <purideepakshi24@gmail.com>
2023-11-04 14:17:19 +0530

    calculator code edited

--- src/main/scala/Calculator.scala
+++ src/main/scala/Calculator.scala
@@ -1,20 +1,20 @@
 object Calculator extends App {
 
   print("Enter 1st number: ")
-  private val n1 = scala.io.StdIn.readInt()
+  val n1 = scala.io.StdIn.readInt()
   print("Enter 2nd number: ")
-  private val n2 = scala.io.StdIn.readInt()
+  val n2 = scala.io.StdIn.readInt()
 
-  private def addition(a: Int, b: Int): Int = {
+  def addition(a: Int, b: Int): Int = {
     a + b
   }
-  private def subtraction(a: Int, b: Int): Int = {
+  def subtraction(a: Int, b: Int): Int = {
     a - b
   }
-  private def multiplication(a: Int, b: Int): Int = {
+  def multiplication(a: Int, b: Int): Int = {
     a * b
   }
-  private def division(a: Int, b: Int): Int = {
+  def division(a: Int, b: Int): Int = {
     if (b != 0) a / b
     else throw new IllegalArgumentException("Division by zero is not allowed.")
   }
